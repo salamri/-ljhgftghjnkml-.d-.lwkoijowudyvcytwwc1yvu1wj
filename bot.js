ed83bbc8221ee66d18e048408d5c62b6a3a71fcb
7asanasddknxbo <42066039+7asanasddknxbo@users.noreply.github.com>
2018-08-09 09:08:46 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,188 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ const prefix = "-";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'help')) {
+    let pages = ['**الأوامر العامة : - \n \`\`\`-id \n-emojis\n-rank	\`\`\`** ','**الأوامر الأدارية : - \n \`\`\` -voiceset \n -giveaway \n	\`\`\`**','']
+    let page = 1;
+
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setFooter(`Page ${page} of ${pages.length}`)
+    .setDescription(pages[page-1])
+
+    message.channel.sendEmbed(embed).then(msg => {
+
+        msg.react('◀').then( r => {
+            msg.react('▶')
+
+
+        const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
+        const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
+
+
+        const backwards = msg.createReactionCollector(backwardsFilter, { time: 20000});
+        const forwards = msg.createReactionCollector(forwardsFilter, { time: 20000});
+
+
+
+        backwards.on('collect', r => {
+            if (page === 1) return;
+            page--;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        forwards.on('collect', r => {
+            if (page === pages.length) return;
+            page++;
+            embed.setDescription(pages[page-1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(embed)
+        })
+        })
+    })
+    }
+});
+
+client.on('message', message => { 
+    if (message.content.startsWith(prefix + 'emojis')) {
+
+        const List = message.guild.emojis.map(e => e.toString()).join(" ");
+
+        const EmojiList = new Discord.RichEmbed()
+            .setTitle(':negative_squared_cross_mark: »  Emojis.') 
+            .setAuthor(message.guild.name, message.guild.iconURL) 
+            .setColor('RANDOM') 
+            .setDescription(List) 
+            .setFooter(message.guild.name) 
+        message.channel.send(EmojiList) 
+    }
+});
+
+
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "voiceset")) {
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply(':x: **ليس لديك الصلاحيات الكافية**');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply(':x: **ليس معي الصلاحيات الكافية**');
+  var args = message.content.split(' ').slice(1).join(' ');
+  if(args && !args.includes(0)) return message.channel.send('**:negative_squared_cross_mark: » فشل اعداد الروم الصوتي .. __يجب عليك كتابة 0 في اسم الروم__**');
+  if(!args) args = ` » VoiceOnline :  ${message.guild.members.filter(s => s.voiceChannel).size} . `;
+  message.channel.send('**:white_check_mark: » تم عمل الروم الصوتي بنجاح**');
+  message.guild.createChannel(`${args.replace(0, message.guild.members.filter(s => s.voiceChannel).size)}`, 'voice').then(c => {
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(() => {
+      c.setName(`${args.replace(0, message.guild.members.filter(s => s.voiceChannel).size)}`).catch(err => {
+        if(err) return;
+      });
+    },3000);
+  });
+  }
+});
+
+
+var userData = {};
+client.on("message", function(message){
+if (message.content.startsWith(prefix + "rank")) {
+	if (!userData[message.author.id]) {
+		userData[message.author.id] = {Money:0,Xp:0,Level:0}
+	}
+     var mentionned = message.mentions.users.first();
+
+      var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+
+      }
+
+	
+	var CulLevel = Math.floor(0.25 * Math.sqrt(userData[message.author.id].Xp +1));
+	if (CulLevel > userData[message.author.id].Level) {userData[message.author.id].Level +=CulLevel}
+	let pEmbed = new Discord.RichEmbed()
+	.setColor("Random")
+	.addField("» UserName :", message.author.tag)
+	.addField("» Level :", userData[message.author.id].Level)
+	.addField("» XP :",Math.floor(userData[message.author.id].Xp))
+	message.channel.send(pEmbed);
+}
+if (!userData[message.author.id]) {
+	userData[message.author.id] = {Money:0,Xp:0,Level:0,Like:0}
+	}
+
+userData[message.author.id].Xp+= 0.25;
+userData[message.author.id].Money+= 0.25;
+
+});
+
+
+client.on('message',async message => {
+var codes = "-";
+var args = message.content.split(" ").slice(1);
+var title = args[1]
+          if(message.content.startsWith(codes + "giveaway")) {
+              if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send('**» ليس لديك البرمشن .**');
+              if(!args) return message.channel.send(`**Use : -giveaway  <Time> <Presentse>**`);
+              if(!title) return message.channel.send(`**Use : **\`-giveaway ${args[0]} Minutes\`** <Presentse>**`);
+         if(!isNaN(args)) return message.channel.send('» **خطء .. الوقت بالدقائق فقط , حاول مره اخري . **');
+                           let giveEmbed = new Discord.RichEmbed()
+                  .setAuthor(message.guild.name, message.guild.iconURL)
+                  .setDescription(`**${title}** \nReact Whith 🎉 To Enter! \n**Time remaining: Minutes :${duration / 60000}**`)
+                  .setFooter(message.author.username, message.author.avatarURL);
+
+                  message.channel.send('**» تم عمل القيف أواي**' , {embed: giveEmbed}).then(m => {
+                      message.delete();
+                      m.react('🎉');
+                     setTimeout(() => {
+                       let users = m.reactions.get("🎉").users;
+                       let list = users.array().filter(u => u.id !== client.user.id);
+                       let gFilter = list[Math.floor(Math.random() * list.length) + 0]
+                       let endEmbed = new Discord.RichEmbed()
+                       .setAuthor(message.author.username, message.author.avatarURL)
+                       .setTitle(title)
+                       .addField('Giveaway End !🎉',`Winners : ${gFilter}`)
+                     m.edit('** 🎉 GIVEAWAY ENDED 🎉**' , {embed: endEmbed});
+                     },args * 60000);
+                   });
+          }
+});
+
+
+
+
+
+
+client.on("message", async message => {
+	if (message.author.bot) return;
+	if (message.channel.type === "dm") return message.reply('** » الأمر في السيرفرات فقط . ** ')
+      if (message.content.startsWith(prefix + 'id')) {
+
+  
+
+				let embed = new Discord.RichEmbed()
+					.setThumbnail(message.author.avatarURL)
+					.setColor("#8A2BE2")
+					.setDescription(`User info for: **${message.author.username}**`)
+					.addField("Avatar:", `${message.author.avatarURL}`, true)
+					.addField("Status:", message.author.presence.status, true)
+					.addField("Bot: ", message.author.bot, true)
+					.addField("Tag: ", message.author.tag, true)
+					.addField("Discriminator:", message.author.discriminator, true)
+					.setFooter(`Created at: ${message.author.createdAt}`)
+
+				message.channel.send(embed)
+      }
+});
+
+
+
+ client.login(process.env.BOT_TOKEN); 
