098d717b7129da574dcd4fd582ede62a82a9bafb
7asanasddknxbo <42066039+7asanasddknxbo@users.noreply.github.com>
2018-08-21 14:22:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const moment = require("moment");
+const fs = require("fs");
 const client = new Discord.Client();
  const prefix = "-";
 client.on('ready', () => {
